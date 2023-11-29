55584341f928fcd12dcbbf07e40daaa6804f16fb
shadyaabed <shady@moltaqa.net>
2023-11-29 15:53:24 +0200

    first-upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mawthok_website
---
+++ locales/en-US.js
@@ -0,0 +1,65 @@
+export default {
+  lang: 'ltr',
+
+  nav: {
+    home: 'HOME',
+    about: 'ABOUT APP',
+    screen: 'APP SCREENS',
+    download: 'DOWNLOAD NOW',
+    contact: 'CONTACT US',
+    terms: 'TERMS & CONDITIONS',
+    policy: 'Privacy Policy',
+    backToHome: 'BACK TO HOME',
+    lang: 'ع',
+  },
+  registerLink: 'REGISTER AS DRIVER',
+  mainSlider: [
+    {
+      mainTitle: 'About Mawthok',
+      title: 'An innovative real estate brokerage application operating in the Kingdom of Saudi Arabia',
+      desc: 'Our mission is to facilitate the process of searching and finding real estate in the Kingdom of Saudi Arabia and facilitate the real estate brokerage process between buyers and sellers. We strive to provide a distinguished and efficient experience for users, where they can access a wide range of available and reliable properties easily and conveniently.',
+    },
+  ],
+  feature: [
+    {
+      image: '1.svg',
+      title: "Wide range of properties",
+      desc: 'Easy and fast booking of boats and yachts through the application, with multiple options for dates, times and routes.',
+    },
+    {
+      image: '2.svg',
+      title: "Advanced search",
+      desc: "Users can customize their search criteria to find the perfect property for them, such as location, size, price, and more.",
+    },
+    {
+      image: '3.svg',
+      title: "Comprehensive information about real estate",
+      desc: 'The app provides detailed information and images of properties, including technical details, surrounding facilities, and real-life photos.'
+    },
+  ],
+  about: {
+    mainTitle: 'Mawthok app is your ideal choice',
+    title: 'About Mawthok',
+    desc: "<p>A multi-service online store application that allows users to purchase products and services online and have them delivered to their address. The application also includes reservation options for restaurants and events, and facilitates the process of obtaining the required services in an easy and convenient way. Those who are looking for a modern and sophisticated shopping experience, as it provides many distinctive and convenient services that make online shopping easy and enjoyable. Therefore, the application can be downloaded easily and benefit from all the services available on it with comfort and confidence. Get the best online shopping experience with the Pring application. Enjoy competitive prices and high quality of the products you buy through the Mawthok app. Fast and reliable delivery for all your purchases, with secure and easy payment options</p>",
+  },
+  downloadText: "<p>You can download the Mawthok app now by visiting the App Store on your smartphone, searching for Mawthok, and then downloading the app on your smartphone.</p>",
+  isoLink: "",
+  androidLink: "",
+  address: 'KSA - Jeddah',
+  terms: {
+    title: 'Terms And Conditions',
+  },
+  policy: {
+    title: 'Privacy Policy',
+  },
+  copyWrite: '©2023 All Rights Reserved For Moltaqa Software House',
+
+  contact: {
+    title: 'Contact Us',
+    name: 'name',
+    email: 'e-mail',
+    phone: 'phone',
+    message: 'message',
+    send: 'send ',
+    },
+}
